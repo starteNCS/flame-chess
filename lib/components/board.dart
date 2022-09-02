3de66dd53a75a8fa1b743da7d0aec23241d47f07
Philipp <philipp.honsel@netgo.de>
2022-09-02 13:40:34 +0200

    Seed white figures

--- lib/components/board.dart
+++ lib/components/board.dart
@@ -1,6 +1,9 @@
 import 'dart:ui';
 
 import 'package:flame/experimental.dart';
+import 'package:flutterwindows/components/figure.dart';
+import 'package:flutterwindows/util/enums/color.dart';
+import 'package:flutterwindows/util/enums/figure.dart';
 
 class BoardComponent extends World {
   static const int boardXSize = 8;
@@ -10,6 +13,11 @@ class BoardComponent extends World {
 
   final whitePaint = Paint()..color = const Color(0xffdddddd);
   final blackPaint = Paint()..color = const Color(0xff111111);
+
+  @override
+  Future<void> onLoad() async {
+    _seedFigures(FigureColor.white);
+  }
 
   @override
   void render(Canvas canvas) {
@@ -22,6 +30,42 @@ class BoardComponent extends World {
             ),
             (x + y) % 2 == 0 ? whitePaint : blackPaint);
       }
+    }
+  }
+
+  void _seedFigures(FigureColor color) {
+    addAll([
+      FigureComponent(
+          figureX: 0, figureY: 0, color: color, figure: Figure.rook),
+      FigureComponent(
+          figureX: 1, figureY: 0, color: color, figure: Figure.knight),
+      FigureComponent(
+          figureX: 2, figureY: 0, color: color, figure: Figure.bishop),
+      FigureComponent(
+          figureX: 3, figureY: 0, color: color, figure: Figure.queen),
+      FigureComponent(
+          figureX: 4, figureY: 0, color: color, figure: Figure.king),
+      FigureComponent(
+          figureX: 5, figureY: 0, color: color, figure: Figure.bishop),
+      FigureComponent(
+          figureX: 6, figureY: 0, color: color, figure: Figure.knight),
+      FigureComponent(
+          figureX: 7, figureY: 0, color: color, figure: Figure.rook),
+    ]);
+
+    _seedPawns(1, FigureColor.white);
+  }
+
+  void _seedPawns(int row, FigureColor color) {
+    for (int x = 0; x < boardXSize; x++) {
+      add(
+        FigureComponent(
+          figureX: x,
+          figureY: row,
+          color: color,
+          figure: Figure.pawn,
+        ),
+      );
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,6 @@ import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterwindows/components/board.dart';
-import 'package:flutterwindows/components/figure.dart';
-import 'package:flutterwindows/util/enums/color.dart';
-import 'package:flutterwindows/util/enums/figure.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,27 +20,19 @@ class ChessGame extends FlameGame {
     await Flame.images.loadAll([
       'figures/black/pawn.png',
       'figures/black/rook.png',
+      'figures/black/knight.png',
+      'figures/black/bishop.png',
+      'figures/black/queen.png',
+      'figures/black/king.png',
       'figures/white/pawn.png',
       'figures/white/rook.png',
+      'figures/white/knight.png',
+      'figures/white/bishop.png',
+      'figures/white/queen.png',
+      'figures/white/king.png',
     ]);
 
-    final pawn = FigureComponent(
-      figureX: 0,
-      figureY: 0,
-      color: FigureColor.white,
-      figure: Figure.rook,
-    );
-
-    final pawn2 = FigureComponent(
-      figureX: 0,
-      figureY: 1,
-      color: FigureColor.white,
-      figure: Figure.pawn,
-    );
-
-    final board = BoardComponent()
-      ..add(pawn)
-      ..add(pawn2);
+    final board = BoardComponent();
 
     final camera = CameraComponent(world: board)
       ..viewfinder.visibleGameSize = Vector2(1600, 1600)
